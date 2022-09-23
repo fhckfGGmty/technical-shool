164a4529ea7de165eb2bd7a172220b51bb633b82
fhckfGGmty <mila.filonenko.202@gmail.com>
2022-09-23 19:04:23 +0300

    deginning

---
+++ conversation.py
@@ -0,0 +1,6 @@
+housing = input("в каком городе ты находишся?")
+print("о, ты живёш в", housing , "?")
+name, surname= input("а как твоё имя?"), input("ну а фамилия какя?")
+print("ясно, буду знать тебя как", name, surname, "каторый живёт в", housing)
+
+
